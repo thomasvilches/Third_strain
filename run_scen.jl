759846450484f8ae5a53fb1abb5ca3750b01e3b0
thomasvilches <thomas_vilches@hotmail.com>
2021-04-16 17:03:30 -0300

    huge change in contact dynamics

---
+++ run_scen.jl
@@ -0,0 +1,12 @@
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"moderna")
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"moderna",1,215,1,1)
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"moderna",2,215,1,14)
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"moderna",3,215,2,1)
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"moderna",4,215,2,14)
+
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"pfizer")
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"pfizer",1,215,1,1)
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"pfizer",2,215,1,14)
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"pfizer",3,215,2,1)
+run_param_scen(0.111,10,50,1.0,1.0,1.5,true,"pfizer",4,215,2,14)
+
