eb160c8e5d3b2c9ba6fe499c3634ac5dc2fee0d6
thomasvilches <thomas_vilches@hotmail.com>
2021-10-13 11:11:55 -0400

    added ID

--- covid19abm.jl
+++ covid19abm.jl
@@ -653,6 +653,7 @@ export _collectdf, _get_incidence_and_prev, _get_column_incidence, _get_column_p
 ## initialization functions 
 function get_province_ag(prov) 
     ret = @match prov begin
+        :idaho => Distributions.Categorical(@SVector [0.065022816741417,0.212624051167697,0.382128238200625,0.177572723991573,0.162652169898689])
         :delaware => Distributions.Categorical(@SVector [0.056193287079826,0.178886259915133,0.365612201724442,0.205312580871751,0.193995670408846])
         :rhodeisland => Distributions.Categorical(@SVector [0.051465930877199,0.173710378237447,0.389835948274479,0.208422813375233,0.176564929235643])
         :newjersey => Distributions.Categorical(@SVector [0.057946294776401,0.184278539414266,0.384542550879907,0.207109733072587,0.166122881856839])
