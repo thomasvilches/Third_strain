510263f570c1a40b02791db57f30c5d05e60803b
thomasvilches <thomas_vilches@hotmail.com>
2021-10-27 20:34:29 -0400

    first commit in branch

--- scen.jl
+++ scen.jl
@@ -1,7 +1,3 @@
 
-red = 0.0:0.05:0.5
-trans = 1.0:0.05:1.5
- 
-for r in red,tr in trans
-    run_param_scen(0.109,10,1,1.0,1.0,1.6,tr,true,r,1,999,2,14)
-end
+
+run_param_scen(0.109,10,1,1.0,1.0,1.0,1.3,true,0.2,4,999,2,14)
--- simulations_cluster.jl
+++ simulations_cluster.jl
@@ -18,7 +18,7 @@ using DelimitedFiles
 
 #@everywhere using covid19abm
 
-addprocs(SlurmManager(500), N=17, topology=:master_worker, exeflags="--project=.")
+addprocs(SlurmManager(250), N=8, topology=:master_worker, exeflags="--project=.")
 @everywhere using Parameters, Distributions, StatsBase, StaticArrays, Random, Match, DataFrames
 @everywhere include("covid19abm.jl")
 @everywhere const cv=covid19abm
@@ -197,7 +197,7 @@ function create_folder(ip::cv.ModelParameters,vac="none")
     
     
     #RF = string("heatmap/results_prob_","$(replace(string(ip.β), "." => "_"))","_vac_","$(replace(string(ip.vaccine_ef), "." => "_"))","_herd_immu_","$(ip.herd)","_$strategy","cov_$(replace(string(ip.cov_val)))") ## 
-    main_folder = "/data/thomas-covid/third_strain_revision"
+    main_folder = "/data/thomas-covid/Alisons_project"
     #main_folder = "."
    
     RF = string(main_folder,"/results_prob_","$(replace(string(ip.β), "." => "_"))","_herd_immu_","$(ip.herd)","_$vac","_$(ip.fourth_strain_trans)_$(ip.strain_ef_red3)_$(ip.file_index)") ##  
